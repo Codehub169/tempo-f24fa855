daf5429f550ac899a1368c839f9a8e71e1bfce5a
Codehub169 <codehub@169pi.com>
2025-05-28 14:18:00 +0530

    Applied user request: PUT THE CONTENTS OF THE STARTUP.SH IN IT
    This code was run inside of a docker container, the container stopped due to some issue or something else happenedThis was the error log: ./startup.sh: line 1

--- startup.sh
+++ startup.sh
@@ -1,1 +1,12 @@
-...
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Run the build process
+echo "Building the application..."
+npm run build
+
+# Serve the built application
+echo "Starting the server..."
+npm run serve
